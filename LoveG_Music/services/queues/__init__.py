41b71632a71d6b016af4bf5c300f4baef30ddb7e
Avnish <89906297+KingNoobX@users.noreply.github.com>
2021-09-09 18:23:57 +0530

    Add files via upload

---
+++ LoveG_Music/services/queues/__init__.py
@@ -0,0 +1,7 @@
+from LoveG_Music.services.queues.queues import clear 
+from LoveG_Music.services.queues.queues import get
+from LoveG_Music.services.queues.queues import is_empty
+from LoveG_Music.services.queues.queues import put
+from LoveG_Music.services.queues.queues import task_done
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
